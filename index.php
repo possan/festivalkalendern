868bce1a6736b53e27a9b5e00507e62fc5383f79
Per-Olov Jernberg <possan@possan.se>
2011-06-01 21:39:16 +0200

    Added Google Analytics

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 		<meta name="viewport" content = "width=device-width, maximum-scale=1, user-scalable=0, user-scalable=no" />
-		<link rel="stylesheet" href="festivalkalendern.css" type="text/css"/>
+		<link rel="stylesheet" href="styles/festivalkalendern.css" type="text/css"/>
 		<style type="text/css">
 			<?php echo ".background { background-image:url(images/".(1+rand()%9).".jpg); };"; ?>
 		</style>
@@ -32,5 +32,18 @@
 				<a href="http://possan.se">blog</a>)
 			</p>
 		</div>
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-23721861-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	</body>
 </html>
