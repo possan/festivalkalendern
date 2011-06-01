592b45ab9039309310dc64b76d8d3f990b9de661
Per-Olov Jernberg <possan@possan.se>
2011-06-01 21:30:23 +0200

    Updated start page

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" zzmanifest="cache.manifest">
+	<head>
+		<script type="text/javascript" src="scripts/ready+loader-release.js"></script>
+		<script type="text/javascript" src="scripts/modernizr-1.7.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery.js"></script>
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+		<meta name="viewport" content = "width=device-width, maximum-scale=1, user-scalable=0, user-scalable=no" />
+		<link rel="stylesheet" href="festivalkalendern.css" type="text/css"/>
+	</head>
+	<body scroll="no">
+			<div class="background" style="background-image:url(images/1.jpg);">
+			</div>
+		<div class="wrapper">
+			<div class="centerbadge">
+				<div class="inner">
+					<h1>Festivalkalendern</h1>
+					<p>
+						&Ouml;ppnar snart...
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<p>
+				<a href="http://twitter.com/possan">@possan</a>
+				(
+				<a href="http://possan.se">blog</a>)
+			</p>
+		</div>
+	</body>
+</html>
